3d7adc2e1a656bb846228254f9c27e072c595270
void <void@gmail.com>
2023-09-04 02:46:39 -0700

    add input dataset validation feedback

--- data-structure-visualizer/scripts/utility-functions.js
+++ data-structure-visualizer/scripts/utility-functions.js
@@ -4,45 +4,60 @@ const parse_input = (input_dirty, dstype, inputtype, InputOptions) => {
 	let input_safe = sanitize_input(input_dirty);
 	if (validate_input(input_safe)) {
 		let obj = deserialize_input(input_safe);
+
+		let msg = '';
+		let invalid = document.querySelector(
+			'.dataset-container .invalid-feedback'
+		);
+		let textarea = document.querySelector('textarea#dataset-textarea');
+
 		if (obj == null) {
-			console.error('invalid input');
+			msg = 'Malformed input data';
+		} else {
+			if (dstype == 'graph') {
+				if (Array.isArray(obj) == false) {
+					msg = 'Input data is not a 2D array';
+				}
+				for (let row of obj) {
+					if (Array.isArray(row) == false) {
+						msg = 'Input data is not a 2D array';
+						break;
+					}
+				}
+				if (inputtype == 'adjacency_matrix') {
+					let height = obj.length;
+					for (let row of obj) {
+						if (row.length != height) {
+							msg = 'Matrix is not square';
+							break;
+						}
+					}
+					return obj;
+				}
+
+				if (inputtype == 'adjacency_list') {
+					let rowsize = InputOptions.graph.weighted ? 3 : 2;
+					for (let row of obj) {
+						if (row.length != rowsize) {
+							msg = `Adjacency list is not N x ${rowsize}`;
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		if (msg) {
+			invalid.innerText = msg;
+			textarea.classList.toggle('is-invalid', true);
+			textarea.classList.toggle('is-valid', false);
 			return null;
+		} else {
+			invalid.innerText = '';
+			textarea.classList.toggle('is-invalid', false);
+			textarea.classList.toggle('is-valid', true);
+			return obj;
 		}
-		if (dstype == 'graph') {
-			if (Array.isArray(obj) == false) {
-				console.error('Input data is not a 2D array');
-				return null;
-			}
-			for (let row of obj) {
-				if (Array.isArray(row) == false) {
-					console.error('Input data is not a 2D array');
-					return null;
-				}
-			}
-			if (inputtype == 'adjacency_matrix') {
-				let height = obj.length;
-				for (let row of obj) {
-					if (row.length != height) {
-						console.error('matrix is not square');
-						return null;
-					}
-				}
-				return obj;
-			}
-
-			if (inputtype == 'adjacency_list') {
-				let rowsize = InputOptions.graph.weighted ? 3 : 2;
-				for (let row of obj) {
-					if (row.length != rowsize) {
-						console.error(`Adjacency list is not N x ${rowsize}`);
-						return null;
-					}
-				}
-				return obj;
-			}
-		}
-
-		return obj;
 	}
 };
 

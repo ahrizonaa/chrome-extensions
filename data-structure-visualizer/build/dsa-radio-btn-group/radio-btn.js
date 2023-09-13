aa439a7dd84209664388a39fea9b28bc77510f4c
missahrizona <princessssahri@gmail.com>
2023-09-13 12:29:42 -0500

    fix styles

--- data-structure-visualizer/build/dsa-radio-btn-group/radio-btn.js
+++ data-structure-visualizer/build/dsa-radio-btn-group/radio-btn.js
@@ -2,7 +2,7 @@ let RadioBtn = document.createElement('button');
 RadioBtn.type = 'button';
 RadioBtn.dataset.teRippleInit = '';
 RadioBtn.dataset.teRippleColor = 'light';
-RadioBtn.setAttribute('class', `inline-block bg-primary px-6 pb-1.5 pt-1.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600`);
+RadioBtn.setAttribute('class', `inline-block flex-grow bg-primary px-6 pb-1.5 pt-1.5 text-xs uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600`);
 RadioBtn.setAttribute('dsa-option', '');
 export { RadioBtn };
 //# sourceMappingURL=radio-btn.js.map

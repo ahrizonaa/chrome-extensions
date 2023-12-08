d9265eaea9db53d2f33935501b859f3550799b55
ahrizona <missahrizona@outlook.com>
2023-12-08 16:56:52 -0600

    wip

--- leetlens/src/app/components/bgpicker/bgpicker.ts
+++ leetlens/src/app/components/bgpicker/bgpicker.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { FloatingControlOptions } from 'src/app/types/FloatingControlOptions';
 
 @Component({
@@ -6,11 +12,64 @@ import { FloatingControlOptions } from 'src/app/types/FloatingControlOptions';
   templateUrl: './bgpicker.html',
   styleUrls: ['./bgpicker.css'],
 })
-export class BgPicker {
+export class BgPicker implements AfterViewInit {
   @Input('options') options!: FloatingControlOptions;
-  color: string = '';
+  @ViewChild('bgselector') bgselector: ElementRef<HTMLDivElement>;
 
-  colorChanged(val: string) {
-    console.log(val);
+  color: string = '';
+  bgSelectorVisible: boolean = false;
+  backgrounds: { name: string; url: string }[];
+
+  constructor() {
+    this.backgrounds = [
+      {
+        name: 'Steel',
+        url: '../../../assets/canvas-backgrounds/300/blueprint_paper-1x300.jpg',
+      },
+      {
+        name: 'Grey',
+        url: '../../../assets/canvas-backgrounds/300/blueprint_paper-2x300.jpg',
+      },
+      {
+        name: 'Dark',
+        url: '../../../assets/canvas-backgrounds/300/blueprint_paper-3x300.jpg',
+      },
+      {
+        name: 'Cement',
+        url: '../../../assets/canvas-backgrounds/300/blueprint_paper-4x300.jpg',
+      },
+      {
+        name: 'Urban',
+        url: '../../../assets/canvas-backgrounds/300/blueprint_paper-5x300.jpg',
+      },
+      {
+        name: 'Vinny',
+        url: '../../../assets/canvas-backgrounds/300/blueprint_paper-6x300.jpg',
+      },
+    ];
+  }
+
+  ngAfterViewInit(): void {
+    document.body.addEventListener('click', (ev: MouseEvent) => {
+      let element = ev.target as HTMLElement;
+
+      if (element.id == 'bgselector') {
+        console.log('exiting');
+        return;
+      }
+      if (element && element.id != 'bgselector') {
+        console.log('hiding');
+        this.bgSelectorVisible = false;
+      }
+    });
+  }
+
+  bgSelected(evt: MouseEvent, bg: any) {
+    console.log(bg);
+    this.bgSelectorVisible = false;
+  }
+
+  bgPickerClicked(evt: MouseEvent) {
+    this.bgSelectorVisible = !this.bgSelectorVisible;
   }
 }
